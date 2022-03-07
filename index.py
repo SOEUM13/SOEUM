fd596f27e5347e7aa87b77f529fd983427953838
seora <seorachoi08@gmail.com>
2022-03-07 17:26:42 +0900

    first_commit

---
+++ index.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify, redirect
+app = Flask(__name__)
+
+import config
+
+from pymongo import MongoClient
+client = MongoClient(config.Mongo_key)
+db = client.SOEUM
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/post", methods=["POST"])
+def post():
+
+    name_receive = request.form['name']
+    pwd_receive = request.form['pwd']
+
+    doc = {
+        'name': name_receive,
+        'pwd': pwd_receive
+    }
+
+    db.user.insert_one(doc)
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
