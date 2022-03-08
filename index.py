f7fd63e920c5c8cd1b0a94e70cb0b2e64cf2d68d
seora <seorachoi08@gmail.com>
2022-03-08 15:34:02 +0900

    like test

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify, redirect
 app = Flask(__name__)
 
+from random import randrange
+
 import certifi
 import config
 
@@ -16,12 +18,36 @@ def home():
 
 @app.route('/like')
 def like():
-    return render_template('likes.html')
+    return render_template('like.html')
 
 
-@app.route('/update_like')
+@app.route('/update_like', methods=['POST'])
 def update_like():
-    return render_template('likes.html')
+    action_receive = request.form["action_give"]
+    #랜덤 숫자
+    number = randrange(10)
+    doc = {
+        "number": number,
+        "like": "test",
+        "id": "test"
+    }
+    if action_receive == "like":
+        db.likes.insert_one(doc)
+    else:
+        db.likes.insert_one(doc)
+    return redirect("/like")
+
+
+@app.route('/top5', methods=['GET'])
+def get_top():
+
+    number_list = list(db.likes.find({}, {'_id':False}).sort("number", -1).limit(5))
+
+    # number_list = list(db.likes.find({}, {'_id':False}))
+
+    # for rows in number_list:
+    #     print(rows)
+    return jsonify({'mynumber':number_list})
 
 
 @app.route("/post", methods=["POST"])
